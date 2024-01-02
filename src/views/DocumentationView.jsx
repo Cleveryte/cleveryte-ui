c8aa8e2a36605124aea9ffa26ce8de7189d02223
LucianoEsplai <lfrancoepifani@gmail.com>
2024-01-02 20:15:40 +0100

    Adding sideBar

--- src/views/DocumentationView.jsx
+++ src/views/DocumentationView.jsx
@@ -1,7 +1,9 @@
+import SideBar from "../components/SideBar";
+
 export default function DocumentationView() {
   return (
     <main className="container px-2 py-4">
-      This is the docs
+      <SideBar/>
     </main>
   )
 }
