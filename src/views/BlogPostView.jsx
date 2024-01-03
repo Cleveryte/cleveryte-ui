f11cedb11b349c29c21334d6b4f078f8a0dc4905
Albert Castiñeira Aranda <albertcastiaran@gmail.com>
2024-01-03 13:07:42 +0100

    Blog content is now ready

--- src/views/BlogPostView.jsx
+++ src/views/BlogPostView.jsx
@@ -1,12 +1,24 @@
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
+import Post1 from "./Posts/Post1";
 
 export default function BlogPostView() {
+  const { id } = useParams();
 
-    const {id} = useParams();
+  let content;
 
-    return (
-        <div className="container mt-4 px-4">
-            Post {id}
-        </div>
-    )
+  switch (id) {
+    case "1":
+      content = <Post1 />;
+      break;
+
+    default:
+      content = <div>Post not found</div>;
+      break;
+  }
+
+  return (
+    <div className="container mt-4 px-4">
+      {content}
+    </div>
+  );
 }
