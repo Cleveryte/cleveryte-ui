eca95c9520bd7082049ff673bcbbad237607dc44
LucianoEsplai <lfrancoepifani@gmail.com>
2024-01-03 12:19:47 +0100

    Adjusted navbar size for improved visibility and aesthetics

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -13,7 +13,7 @@ const toggleSideBar = () =>{
   return (
     <div className='container flex'>
          <div className='block md:hidden'>
-                <Icon className={`${mobileSideBar ? '' : 'hidden'} text-4xl text-[#003466] rounded-lg`} onClick={toggleSideBar} icon="lucide:panel-left" />
+                <Icon className={`${mobileSideBar ? '' : 'hidden'} text-3xl text-[#003466] rounded-lg m-0 w-[30px] h-[30px]`} onClick={toggleSideBar} icon="lucide:panel-left" />
             </div>
         <aside className={`${mobileSideBar ? "hidden md:block" : ""} h-[85vh] w-[250px] overflow-auto border-b border-b-gray-100`}>
             <nav>
@@ -28,7 +28,7 @@ const toggleSideBar = () =>{
                         <li>Custimization</li>
                 
                 </ul>
-                <ul className='mt-6'>
+                <ul className='mt-3 md:mt-6'>
                     <div className='flex items-center gap-2'>
                         <Icon className='text-2xl' icon="material-symbols:dashboard" color='#003366' />
                         <h2 className='text-xl font-semibold'>Components</h2>
@@ -50,7 +50,7 @@ const toggleSideBar = () =>{
                         <li>Toast</li>
                 
                 </ul>
-                <ul className='mt-6'>
+                <ul className='mt-3 md:mt-6'>
                     <div className='flex items-center gap-2'>
                         <Icon className='text-2xl' icon="jam:world" color='#003366' />
                         <h2 className='text-xl font-semibold'>About</h2>
