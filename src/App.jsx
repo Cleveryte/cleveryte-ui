8b9d7d2fa28a4b9499078e2cdc0d4cfda85656d9
Albert Castiñeira Aranda <albertcastiaran@gmail.com>
2024-01-03 12:12:09 +0100

    Header fix + Blog basic links

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import DocumentationView from './views/DocumentationView';
 import BlogView from './views/BlogView';
+import BlogPostView from './views/BlogPostView';
 import { BrowserRouter as BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<DocumentationView />} />
           <Route path="/blog" element={<BlogView />} />
+          <Route path="/blog/:id" element={<BlogPostView />} />
         </Routes>
       </BrowserRouter>
     </>
