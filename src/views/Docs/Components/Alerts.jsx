745b105087e5e697f7f5b81437a12bc51acc2d3e
LucianoEsplai <lfrancoepifani@gmail.com>
2024-01-09 12:22:35 +0100

    Creating AlertsCards but tailwindcss styles are not working

--- src/views/Docs/Components/Alerts.jsx
+++ src/views/Docs/Components/Alerts.jsx
@@ -1,72 +1,10 @@
 import SideBar from '../../../components/SideBar';
 import CustomCodeBlock from '../../../components/CustomCodeBlock';
+import AlertsCard from '../../../code/AlertsCard.json'
+import { Icon } from '@iconify/react';
 
 export default function Alerts() {
-  const alerts = [
-    {
-      htmlCode: `<div class='bg-blue-200 border border-[#759BFF] rounded-full p-2 w-96 text-black flex items-center gap-1'>
-                  <iconify-icon class='text-xl' icon="material-symbols:info-outline-rounded"></iconify-icon>
-                  <p class='text-sm font-bold'>New update is available</p>
-                </div>`,
-      jsxCode: `<div className='bg-blue-200 border border-[#759BFF] rounded-full p-2 w-96 text-black flex items-center gap-1'>
-                  <Icon icon="material-symbols:info-outline-rounded" className='text-2xl mx-2' />
-                  <p className='text-sm font-bold'>New update is available</p>
-                </div>`,
-    },
-    {
-      htmlCode: `<div class='bg-green-200 border border-[#00FF00]  rounded-full p-2 w-96 text-black flex items-center gap-1'>
-                  <iconify-icon class='text-xl' icon="clarity:success-standard-line"></iconify-icon>
-                  <p class='text-sm font-bold'>The update was successfully installed!</p>
-               </div>`,
-      jsxCode: `<div className='bg-green-200 border border-[#00FF00] rounded-full p-2 w-96 text-black flex items-center gap-1'>
-                  <Icon icon="clarity:success-standard-line" className='text-2xl mx-2' />
-                  <p className='text-sm font-bold'>The update was successfully installed!</p>
-                </div>`,
-    },
-    {
-      htmlCode: `<div class='bg-[#FFEAAE] border border-[#FFBE00] rounded-full p-2 w-96 text-black flex items-center gap-1'>
-                  <iconify-icon class='text-xl' icon="ph:warning-bold"></iconify-icon>
-                  <p class='text-sm font-bold'>The update was successfully installed!</p>
-               </div>`,
-      jsxCode: `<div className='bg-green-200 border border-[#00FF00] rounded-full p-2 w-96 text-black flex items-center gap-1'>
-                  <Icon icon="ph:warning-bold" className='text-2xl mx-2' />
-                  <p className='text-sm font-bold'>The update was successfully installed!</p>
-                </div>`,
-    },
-    {
-      htmlCode: `<div class='bg-[#FF9898] border border-[#FD0000]  rounded-full p-2 w-96 text-black flex items-center gap-1'>
-                  <iconify-icon class='text-xl' icon="codicon:error"></iconify-icon>
-                  <p class='text-sm font-bold'>This action may have undesired consequences.</p>
-               </div>`,
-      jsxCode: `<div className='bg-[#FF9898] border border-[#FD0000] rounded-full p-2 w-96 text-black flex items-center gap-1'>
-                  <Icon icon="codicon:error" className='text-2xl mx-2' />
-                  <p className='text-sm font-bold'>This action may have undesired consequences.</p>
-                </div>`,
-    },
-    {
-      htmlCode: `<div class='bg-[#f77fbf] border border-[#C71585]  rounded-full p-2 w-96 text-black flex items-center gap-1'>
-                  <iconify-icon class='text-xl' icon="mingcute:notification-line"></iconify-icon>
-                  <p class='text-sm font-bold'>You have a new message!</p>
-               </div>`,
-      jsxCode: `<div className='bg-[#f77fbf] border border-[#C71585] rounded-full p-2 w-96 text-black flex items-center gap-1'>
-                  <Icon icon="mingcute:notification-line" className='text-2xl mx-2' />
-                  <p className='text-sm font-bold'>You have a new message!</p>
-                </div>`,
-    },
-    {
-      htmlCode: `<div class='bg-[#f77fbf] border border-[#C71585]  rounded-full p-2 w-96 text-black flex items-center gap-1'>
-                  <iconify-icon class='text-xl' icon="mflat-color-icons:ok"></iconify-icon>
-                  <p class='text-sm font-bold'>You have a new message!</p>
-               </div>`,
-      jsxCode: `<div className='bg-[#f77fbf] border border-[#C71585] rounded-full p-2 w-96 text-black flex items-center gap-1'>
-                  <Icon icon="mflat-color-icons:ok" className='text-2xl mx-2' />
-                  <p className='text-sm font-bold'>You have a new message!</p>
-                </div>`,
-    },
-   
   
-    
-  ];
 
   return (
     <div className='container px-2 py-4'>
@@ -77,11 +15,36 @@ export default function Alerts() {
           <p className="text-[#9A9A9A] mb-6">Use custom buttons styles for actions in alerts, delete, and more.</p>
 
           <h3 className="mb-4">Alerts</h3>
-          {alerts.map((alert, index) => (
-            <div key={index} className='my-4'>
-              <CustomCodeBlock htmlCode={alert.htmlCode} jsxCode={alert.jsxCode} />
+          
+            <div className='my-4'>
+            <CustomCodeBlock 
+                  htmlPreview={AlertsCard.info_alert.htmlPreview} 
+                  htmlFormatted={AlertsCard.info_alert.htmlFormatted} 
+                  jsxFormatted={AlertsCard.info_alert.jsxFormatted} 
+                />  
+                <CustomCodeBlock 
+                  htmlPreview={AlertsCard.success_alert.htmlPreview} 
+                  htmlFormatted={AlertsCard.success_alert.htmlFormatted} 
+                  jsxFormatted={AlertsCard.success_alert.jsxFormatted} 
+                />   
+                <CustomCodeBlock 
+                  htmlPreview={AlertsCard.warning_alert.htmlPreview} 
+                  htmlFormatted={AlertsCard.warning_alert.htmlFormatted} 
+                  jsxFormatted={AlertsCard.warning_alert.jsxFormatted} 
+                />  
+                 <CustomCodeBlock 
+                  htmlPreview={AlertsCard.error_alert.htmlPreview} 
+                  htmlFormatted={AlertsCard.error_alert.htmlFormatted} 
+                  jsxFormatted={AlertsCard.error_alert.jsxFormatted} 
+                />  
+                 <CustomCodeBlock 
+                  htmlPreview={AlertsCard.message_alert.htmlPreview} 
+                  htmlFormatted={AlertsCard.message_alert.htmlFormatted} 
+                  jsxFormatted={AlertsCard.message_alert.jsxFormatted} 
+                />  
+                
             </div>
-          ))}
+          
         </div>
       </div>
     </div>
